9de4e349f7f37a330c88138193a10cb6df5ac98f
abdurrafaysaqlain <abdurrafaysaqlain@hotmail.com>
2022-12-29 00:23:01 +0500

    bnrgradientio

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Initialize Wow
+$( document ).ready(function() {
+  new WOW().init()
+});
+//Read slider
+$(document).ready(function(){
+
+  $('.demo').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 2000,
+});
+  // $('.read__slider').slick(
+  // //   {
+  // //   infinite: true,
+  // //   slidesToShow: 3,
+  // //   slidesToScroll: 3,
+  // // }
+  // );
+});
+
+// $('.get-started-btn').css({
+//   opacity: function() {
+//     var elementHeight = $(this).height(),
+//         opacity = ((1 - (100 - 0) / 100) * 1) + 1;
+
+//     return opacity;
+//   }
+// });
+$(window).scroll(function(){
+  var st = $(window).scrollTop();
+  var range = 300 // finetune this to the desired effect
+  $('.get-started-btn').css("opacity", 1- st / range); // animate your element
+});
